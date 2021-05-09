5a9286c9ea44a27a5b9b6c45b49f5f0c3a417d90
ariake <kyh31876@gmail.com>
2021-05-09 13:45:27 +0900

    'main'

--- 28:4.js
+++ 28:4.js
@@ -195,3 +195,198 @@ return sum;
 }
 // Now invoke the timed version of that test function
 timed(benchmark)(1000000) // => 500000500000; this is the sum of the numbers
+
+
+
+    //Destructuring Function Arguments into Parameters
+function vectorAdd(v1, v2) {
+    return [v1[0] + v2[0], v1[1] + v2[1]];
+    }
+
+
+    function vectorAdd([x1,y1], [x2,y2]) { // Unpack 2 arguments into 4 parameters
+        return [x1 + x2, y1 + y2];
+        }
+        vectorAdd([1,2], [3,4]) // => [4,6]
+
+// Multiply the vector {x,y} by a scalar value
+function vectorMultiply({x, y}, scalar) {
+    return { x: x*scalar, y: y*scalar };
+    }
+    vectorMultiply({x: 1, y: 2}, 2) // => {x: 2, y: 4}
+
+function vectorAdd(
+    {x: x1, y: y1}, // Unpack 1st object into x1 and y1 params
+    {x: x2, y: y2} // Unpack 2nd object into x2 and y2 params
+    )
+    {
+    return { x: x1 + x2, y: y1 + y2 };
+    }
+vectorAdd({x:1,y:2},{x:1,y:4})
+
+// Multiply the vector {x,y} or {x,y,z} by a scalar value
+function vectorMultiply({x, y, z=0}, scalar) {
+    return { x: x*scalar, y: y*scalar, z: z*scalar };
+    }
+
+vectorMultiply({x:1,y:2,},2)
+
+function arraycopy({from, to=from, n=from.length,
+    fromIndex=0, toIndex=0}) {
+    let valuesToCopy = from.slice(fromIndex, fromIndex + n);
+    to.splice(toIndex, 0, ...valuesToCopy);
+    return to;
+    }
+let a = [1,2,3,4,5], b = [9,8,7,6,5];
+arraycopy({from:a,n:3,to:b,toIndex:4})
+
+// This function expects an array argument. The first two elements of that
+// array are unpacked into the x and y parameters. Any remaining elements
+// are stored in the coords array. And any arguments after the first array
+// are packed into the rest array.
+function f([x, y, ...coords], ...rest) {
+return [x+y, ...rest, ...coords]; // Note: spread operator here
+}
+f([1, 2, 3, 4], 5, 6) // => [3, 5, 6, 3, 4]
+f([1,2,3,4,5],3,4)
+
+
+// Multiply the vector {x,y} or {x,y,z} by a scalar value, retain other props
+function vectorMultiply({x, y, z=0, ...props}, scalar) {
+return { x: x*scalar, y: y*scalar, z: z*scalar, ...props
+};
+}
+vectorMultiply({x: 1, y: 2, w: -1}, 2) // => {x: 2, y: 4, z: 0, w: -1}
+
+vectorMultiply({x:2,y:3,w:2},3)
+
+function drawCircle({x, y, radius, color: [r, g, b]}) {
+    // Not yet implemented
+    }
+
+
+// Return the sum of the elements an iterable object a.
+// The elements of a must all be numbers.
+function sum(a) {
+    let total = 0;
+    for(let element of a) { // Throws TypeError if a is not iterable
+    if (typeof element !== "number") {
+    throw new TypeError("sum(): elements must be numbers");
+    }
+    total += element;
+    }
+    return total;
+    }
+
+sum(1,2,3);
+sum([1,2,3]);
+
+function square(x) {
+    return x*x
+}; 
+
+let s=square;
+square(4)
+
+
+let o = {square: function(x) { return x*x; }}; // An object literal
+let y = o.square(16);
+
+let a = [x => x*x, 20]; // An array literal
+a[0](a[1]) // => 400
+
+
+//Example 8.1
+function add(x,y) { return x + y; }
+function subtract(x,y) { return x - y; }
+function multiply(x,y) { return x * y; }
+function divide(x,y) { return x / y; }
+// Here's a function that takes one of the preceding functions
+// as an argument and invokes it on two operands
+function operate(operator, operand1, operand2) {
+return operator(operand1, operand2);
+}
+// We could invoke this function like this to compute the value (2+3) + (4*5):
+let i = operate(add, operate(add, 2, 3), operate(multiply, 4,
+5));
+// For the sake of the example, we implement the simple functions again,
+// this time within an object literal;
+const operators = {
+add: (x,y) => x+y,
+subtract: (x,y) => x-y,
+multiply: (x,y) => x*y,
+divide: (x,y) => x/y,
+pow: Math.pow // This works for predefined functions too
+};
+// This function takes the name of an operator, looks up that operator
+// in the object, and then invokes it on the supplied operands Note.
+// the syntax used to invoke the operator function.
+function operate2(operation, operand1, operand2) {
+if (typeof operators[operation] === "function") {
+return operators[operation](operand1, operand2);
+}
+else throw "unknown operator";
+}
+operate2("add", "hello", operate2("add", " ", "world")) // =>"hello world"
+operate2("pow", 10, 2) // => 100
+
+
+
+// This function returns a different integer each time it iscalled.
+// It uses a property of itself to remember the next value to be returned.
+function uniqueInteger() {
+    return uniqueInteger.counter++; // Return and increment counter property
+}
+// Initialize the counter property of the function object.
+// Function declarations are hoisted so we really can
+// do this assignment before the function declaration.
+uniqueInteger.counter = 0;
+
+
+uniqueInteger() // => 0
+uniqueInteger() // => 1
+
+
+
+// Compute factorials and cache results as properties of the function itself.
+
+function factorial(n) {
+if (Number.isInteger(n) && n > 0) { // Positive integers only
+if (!(n in factorial)) { // If no cached result
+factorial[n] = n * factorial(n-1); // Compute and cache it
+}
+return factorial[n]; // Return the cached result
+} else {
+return NaN; // If input was bad
+}
+}
+factorial[1] = 1; // Initialize the cache to hold this base case.
+factorial(6) // => 720
+factorial[5] // => 120; the call above caches this value
+
+
+
+let scope = "global scope"; // A global variable
+function checkscope() {
+let scope = "local scope"; // A local variable
+function f() { return scope; } // Return the value in scope here
+return f();
+}
+checkscope() // => "local scope"
+
+let scope = "global scope"; // A global variable
+function checkscope() {
+let scope = "local scope"; // A local variable
+function f() { return scope; } // Return the value in scope here
+return f;
+}
+let s = checkscope()(); // What does this return?
+
+
+
+let uniqueInteger = (function() { // Define and invoke
+    let counter = 0; // Private state of function below
+    return function() { return counter++; };
+    }());
+    uniqueInteger() // => 0
+    uniqueInteger() // => 1
