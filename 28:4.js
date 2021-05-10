1b93e6f98935906f251c24331d52b22bc45e308f
ariake <kyh31876@gmail.com>
2021-05-10 21:05:40 +0900

    'main'

--- 28:4.js
+++ 28:4.js
@@ -350,6 +350,7 @@ uniqueInteger() // => 1
 
 // Compute factorials and cache results as properties of the function itself.
 
+
 function factorial(n) {
 if (Number.isInteger(n) && n > 0) { // Positive integers only
 if (!(n in factorial)) { // If no cached result
@@ -374,6 +375,7 @@ return f();
 }
 checkscope() // => "local scope"
 
+
 let scope = "global scope"; // A global variable
 function checkscope() {
 let scope = "local scope"; // A local variable
@@ -390,3 +392,92 @@ let uniqueInteger = (function() { // Define and invoke
     }());
     uniqueInteger() // => 0
     uniqueInteger() // => 1
+
+
+function counter() {
+    let n = 0;
+    return {
+    count: function() { return n++; },
+    reset: function() { n = 0; }
+    };
+    }
+    let c = counter(), d = counter(); // Create two counters
+    c.count() // => 0
+    d.count() // => 0: they count independently
+    c.reset(); // reset() and count() methods share state
+    c.count() // => 0: because we reset c
+    d.count() // => 1: d was not res
+
+
+
+function counter(n) { // Function argument n is the private variable
+    return {
+    // Property getter method returns and increments private counter var.
+    get count() { return n++; },
+    // Property setter doesn't allow the value of n to decrease
+    set count(m) {
+    if (m > n) n = m;
+    else throw Error("count can only be set to a larger value");
+    }
+    };
+    }
+    let c = counter(1000);
+    c.count // => 1000
+    c.count // => 1001
+    c.count = 2000;
+    c.count // => 2000
+    c.count = 2000; // !Error: count can only be set to a larger value
+
+// This function adds property accessor methods for a property
+with
+// the specified name to the object o. The methods are named get<name>
+// and set<name>. If a predicate function is supplied, the setter
+// method uses it to test its argument for validity before storing it.
+// If the predicate returns false, the setter method throws an exception.
+//
+// The unusual thing about this function is that the property value
+// that is manipulated by the getter and setter methods is not stored in
+// the object o. Instead, the value is stored only in a local variable
+// in this function. The getter and setter methods are also defined
+// locally to this function and therefore have access to this local variable.
+// This means that the value is private to the two accessor methods, and it
+// cannot be set or modified except through the setter method.
+
+
+function addPrivateProperty(o, name, predicate) {
+    let value; // This is the property value
+    // The getter method simply returns the value.
+    o[`get${name}`] = function() { return value; };
+    // The setter method stores the value or throws an exception if
+    // the predicate rejects the value.
+    o[`set${name}`] = function(v) {
+    if (predicate && !predicate(v)) {
+    throw new TypeError(`set${name}: invalid value
+    ${v}`);
+    } else {
+    value = v;
+    }
+    };
+}
+// The following code demonstrates the addPrivateProperty() method.
+let o = {}; // Here is an empty object
+// Add property accessor methods getName and setName()
+// Ensure that only string values are allowed
+addPrivateProperty(o, "Name", x => typeof x === "string");
+o.setName("Frank"); // Set the property value
+o.getName() // => "Frank"
+o.setName(0); // !TypeError: try to set a value of the wrong type
+
+function constfunc(v) { return () => v; }
+// Create an array of constant functions:
+let funcs = [];
+for(var i = 0; i < 10; i++) funcs[i] = constfunc(i);
+
+// Return an array of functions that return the values 0-9
+function constfuncs() {
+    let funcs = [];
+    for(var i = 0; i < 10; i++) {
+    funcs[i] = () => i;
+    }
+    return funcs;
+    }
