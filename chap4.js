1203c4a71a81f576b6f9c4fff15a05812e2b1454
ariake <kyh31876@gmail.com>
2021-05-11 10:26:16 +0900

    'main'

---
+++ chap4.js
@@ -0,0 +1,15 @@
+//Primary Expression
+    true // Evalutes to the boolean true value
+    false // Evaluates to the boolean false value
+    null // Evaluates to the null value
+    this // Evaluates to the "current" object
+
+    i // Evaluates to the value of the variable i.
+    sum // Evaluates to the value of the variable sum.
+    undefined // The value of the "undefined" property of the global object
+
+
+//Object and Array Initializers
+    [] // An empty array: no expressions inside brackets means no elements
+    let matrix =[[1,2,3],[4,5,6],[7,8,9]]
+    let sparseArray = [1,,,,5]; //> sparseArray [ 1, <3 empty items>, 5 ]
